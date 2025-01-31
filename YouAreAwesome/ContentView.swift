1b98e20c2cf9c42cc1a89a79b9387bff1a27fcc3
Matteo <matteo.linux@gmail.com>
2025-01-31 11:31:04 +0100

    2 buttons with spacers

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -13,19 +13,37 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Spacer()
+            
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
+                .frame(width: 200, height: 200)
                 .foregroundStyle(.orange)
-            Text(message)
-                .foregroundStyle(.red)
-                .font(.largeTitle)
-                .fontWeight(.bold)
             
-            Button("Click Me!") {
-                message = "You clicked me!"
-            }
-        }
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome!") {
+                    message = "Awesome!"
+                }
+                
+                Button("Great") {
+                    message = "Great!"
+                }
+                
+            } // end Hstack
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
+            
+            
+            
+        }// end Vstack
         .padding()
     }
 }
